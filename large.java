9a63296a5e5f7366a38a1a2e0d27ae23401c6652
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-07-27 15:59:02 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class large
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      Scanner sc=new Scanner(System.in);
+      int num1,num2,num3;
+      num1=sc.nextInt();
+      num2=sc.nextInt();
+      num3=sc.nextInt();
+      if((num1>num2)&&(num1>num2))
+      System.out.println(num1);
+      else if((num2>num1)&&(num2>num3))
+      System.out.println(num2);
+      else 
+      System.out.println(num3);
+		
+	}
+}
